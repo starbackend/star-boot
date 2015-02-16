fb36faba1c17aabfa5b0151679064fe39580cbd3
maprohu <maprohu@gmail.com>
2015-02-16 16:30:55 +0000

    ignore conf not found

--- app/src/main/java/star/app/StarAppConfiguration.java
+++ app/src/main/java/star/app/StarAppConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource({"classpath:/star-app.properties", "file:conf/star-app.properties"})
+@PropertySource(value={"classpath:/star-app.properties", "file:conf/star-app.properties"}, ignoreResourceNotFound=true)
 @Import(CwatchEnvConfiguration.class)
 public class StarAppConfiguration {
 

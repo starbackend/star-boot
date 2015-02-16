bc8ca5923e7684fb4681c3ac5e4c3a2e912ab1e6
maprohu <maprohu@gmail.com>
2015-02-16 17:26:13 +0000

    disable security

--- app/src/main/java/star/auto/StarMain.java
+++ app/src/main/java/star/auto/StarMain.java
@@ -2,12 +2,18 @@ package star.auto;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ManagementSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
 import star.app.StarAppConfiguration;
+
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={
+		SecurityAutoConfiguration.class, 
+		ManagementSecurityAutoConfiguration.class
+})
 @Import(StarAppConfiguration.class)
 public class StarMain implements CommandLineRunner {
 

e4d741e9daf1fb3f12b73adfbc5755ca742cca9f
maprohu <maprohu@gmail.com>
2015-02-23 17:08:41 +0000

    jolokia security

--- base/app/src/main/java/star/auto/StarMain.java
+++ base/app/src/main/java/star/auto/StarMain.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SpringBootWebSecurityConfiguration;
 import org.springframework.boot.context.config.ConfigFileApplicationListener;
 import org.springframework.context.annotation.Import;
 
--- base/webapp/src/main/java/star/webapp/StarWebappConfiguration.java
+++ base/webapp/src/main/java/star/webapp/StarWebappConfiguration.java
@@ -1,26 +1,59 @@
 package star.webapp;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
 @PropertySource("classpath:/star-webapp.properties")
+@Import(JolokiaAdminConfiguration.class)
 public class StarWebappConfiguration {
 
-	@Configuration
-	@EnableWebMvcSecurity
-	public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-		
-	    public WebSecurityConfig() {
-			super(true);
-		}
+//	@Configuration
+//	@EnableWebMvcSecurity
+//	public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//		
+//	    public WebSecurityConfig() {
+//			super(false);
+//		}
+//
+////		@Override
+////	    protected void configure(HttpSecurity http) throws Exception {
+////	    }
+//	}
 
-		@Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	    }
-	}
-
+//	@Configuration
+//	public static class StarWebappManagementSecurityConfiguration extends ManagementSecurityAutoConfiguration {
+//		
+//		@Configuration
+//		public static class StarWebappManagementWebSecurityConfigurerAdapter extends ManagementWebSecurityConfigurerAdapter {
+//			
+//			@Autowired
+//			private ManagementServerProperties managementProperties;
+//			
+//			@Autowired
+//			private JolokiaMvcEndpoint jolokiaMvcEndpoint;
+//
+//			@Autowired(required=false)
+//			private EndpointHandlerMapping endpointHandlerMappingLocal;
+//			
+//			@Override
+//			public void setEndpointHandlerMapping(
+//					EndpointHandlerMapping endpointHandlerMapping) {
+//				super.setEndpointHandlerMapping(endpointHandlerMapping);
+//				this.endpointHandlerMappingLocal = endpointHandlerMapping;
+//			}
+//			
+//			@Override
+//			protected void configure(HttpSecurity http) throws Exception {
+//				if (managementProperties.getSecurity().isEnabled()) {
+//					http.authorizeRequests().antMatchers(endpointHandlerMappingLocal.getPath(jolokiaMvcEndpoint.getPath())+"/**").permitAll();
+//				}
+//				super.configure(http);
+//			}
+//			
+//		}
+//		
+//	}
+	
 }

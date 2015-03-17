8a0485407caba71201dc68924b68cb5f902c0f9f
maprohu <maprohu@gmail.com>
2015-03-17 16:03:29 +0000

    exclude jms autoconfig

--- base/app/src/main/java/star/auto/StarMain.java
+++ base/app/src/main/java/star/auto/StarMain.java
@@ -1,22 +1,20 @@
 package star.auto;
 
 import java.io.File;
-import java.nio.file.Files;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SpringBootWebSecurityConfiguration;
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 import org.springframework.boot.context.config.ConfigFileApplicationListener;
 import org.springframework.context.annotation.Import;
 
-import com.google.common.base.Throwables;
-
-import ch.qos.logback.core.util.FileUtil;
 import star.app.StarAppConfiguration;
 
-@SpringBootApplication
+import com.google.common.base.Throwables;
+
+@SpringBootApplication(exclude=JmsAutoConfiguration.class)
 @Import(StarAppConfiguration.class)
 public class StarMain implements CommandLineRunner {
 
